f173399fcdef8017e45cd1f698b7e8f9351b58e5
RUzai <RUzairov@gmail.com>
2022-10-08 18:30:40 +0300

    router animation fix

--- lib/main.dart
+++ lib/main.dart
@@ -10,69 +10,103 @@ import 'presentation/pages/people/people_page.dart';
 
 void main() {
   configureDependencies();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'VTB NFT',
-      routes: routes,
-      initialRoute: routes.keys.first,
+      onGenerateRoute: routeByName,
+      initialRoute: LoginPage.name,
       theme: theme,
     );
   }
 
-  final Map<String, Widget Function(BuildContext)> routes = {
-    LoginPage.name: (_) => BlocProvider(
-          create: (_) => sl<LoginBloc>(),
-          child: const LoginPage(),
-        ),
-    HomePage.name: (_) => BlocProvider(
-          create: (_) => sl<HomeBloc>(),
-          child: const HomePage(),
-        ),
-    PeoplePage.name: (_) => BlocProvider(
-          create: (_) => sl<PeopleBloc>(),
-          child: const PeoplePage(),
-        ),
-  };
+  NoAnimationMaterialPageRoute router(Widget child) =>
+      NoAnimationMaterialPageRoute(builder: (_) => child);
 
-  final ThemeData theme = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: Styles.blue,
-    scaffoldBackgroundColor: Styles.bg,
-    iconTheme: const IconThemeData(color: Styles.icon),
-    textTheme: GoogleFonts.ptSansTextTheme().apply(
-      bodyColor: Colors.white,
-      // displayColor: Colors.red,
-    ),
-    appBarTheme: const AppBarTheme(
-      color: Colors.transparent,
-      shadowColor: Colors.transparent,
-      // iconTheme: IconThemeData(color: Colors.black),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      filled: true,
-      fillColor: Styles.inputBg,
-      hoverColor: Styles.inputBg,
-      border: OutlineInputBorder(
-        borderSide: BorderSide.none,
-        borderRadius: Styles.borderRadius,
-      ),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        padding: const MaterialStatePropertyAll(
-          EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+  NoAnimationMaterialPageRoute? routeByName(RouteSettings settings) {
+    switch (settings.name) {
+      case LoginPage.name:
+        return router(
+          BlocProvider(
+            create: (_) => sl<LoginBloc>(),
+            child: const LoginPage(),
+          ),
+        );
+      case HomePage.name:
+        return router(
+          BlocProvider(
+            create: (_) => sl<HomeBloc>(),
+            child: const HomePage(),
+          ),
+        );
+      case PeoplePage.name:
+        return router(
+          BlocProvider(
+            create: (_) => sl<PeopleBloc>(),
+            child: const PeoplePage(),
+          ),
+        );
+    }
+    return null;
+  }
+
+  ThemeData get theme => ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Styles.blue,
+        scaffoldBackgroundColor: Styles.bg,
+        iconTheme: const IconThemeData(color: Styles.icon),
+        textTheme: GoogleFonts.ptSansTextTheme().apply(
+          bodyColor: Colors.white,
+          // displayColor: Colors.red,
         ),
-        shape: MaterialStatePropertyAll(
-          RoundedRectangleBorder(borderRadius: Styles.borderRadius),
+        appBarTheme: const AppBarTheme(
+          color: Colors.transparent,
+          shadowColor: Colors.transparent,
+          // iconTheme: IconThemeData(color: Colors.black),
         ),
-      ),
-    ),
-  );
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Styles.inputBg,
+          hoverColor: Styles.inputBg,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: Styles.borderRadius,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            padding: const MaterialStatePropertyAll(
+              EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            ),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: Styles.borderRadius),
+            ),
+          ),
+        ),
+      );
+}
+
+class NoAnimationMaterialPageRoute<T> extends MaterialPageRoute<T> {
+  NoAnimationMaterialPageRoute({
+    required super.builder,
+    super.settings,
+    super.maintainState,
+    super.fullscreenDialog,
+  });
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return child;
+  }
 }
