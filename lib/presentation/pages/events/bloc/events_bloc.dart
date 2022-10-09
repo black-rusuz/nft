18d7fa3f41fe9240ce9bfacda985a9c91f2ee000
RUzai <RUzairov@gmail.com>
2022-10-09 09:48:46 +0300

    temp events + page

--- lib/presentation/pages/events/bloc/events_bloc.dart
+++ lib/presentation/pages/events/bloc/events_bloc.dart
@@ -2,6 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../../../data/model/enums.dart';
 import '../../../../data/model/event.dart';
 import '../../../../domain/repository.dart';
 
@@ -17,7 +18,40 @@ class EventsBloc extends Bloc<EventsEvent, EventsState> {
   }
 
   void _init(EventsInit event, Emitter<EventsState> emit) async {
-    final events = await _repository.getEvents();
-    emit(EventsSuccess(events: events));
+    //final events = await _repository.getEvents();
+    //emit(EventsSuccess(events: events));
+
+    emit(EventsSuccess(events: [
+      EventModel(
+        title: 'Залупа единорога',
+        description:
+            'Задача заключается в том, чтобы профессионально муравью хуй приделать',
+        imageUrl: 'https://i.imgur.com/YDeLdaH.jpeg',
+        dateTime: DateTime.now(),
+        place: 'ул. Пушкина, д. Колотушкина',
+        price: 1488,
+        currency: Currency.digitalRouble,
+      ),
+      EventModel(
+        title: 'Говно мамонта',
+        description:
+            'Задача заключается в том, чтобы профессионально муравью хуй убрать',
+        imageUrl: 'https://i.imgur.com/jAsUVh4.jpeg',
+        dateTime: DateTime.now(),
+        place: 'Офис в Замоскворечье',
+        price: 3,
+        currency: Currency.nft,
+      ),
+      EventModel(
+        title: 'Жопа коня',
+        description:
+            'Задача заключается в том, чтобы профессионально муравью хуй null',
+        imageUrl: 'https://i.imgur.com/4WNgyr5.jpeg',
+        dateTime: DateTime.now(),
+        place: 'В пизде блядь',
+        price: 1488,
+        currency: Currency.digitalRouble,
+      ),
+    ]));
   }
 }
--- lib/presentation/pages/events/events_page.dart
+++ lib/presentation/pages/events/events_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../data/model/event.dart';
 import '../../core/navigation_page_wrapper.dart';
+import '../../widgets/base_header.dart';
+import '../../widgets/base_text_field.dart';
+import '../home/widgets/events.dart';
 import 'bloc/events_bloc.dart';
 
 export 'bloc/events_bloc.dart';
@@ -18,11 +22,56 @@ class EventsPage extends StatelessWidget {
       body: BlocBuilder<EventsBloc, EventsState>(
         builder: (context, state) {
           if (state is EventsSuccess) {
-            print(state.events);
+            return ListView(
+              children: [
+                const BaseHeader(
+                    'Посещайте мероприятия, чтобы заработать монеты или NFT'),
+                const BaseTextField(hint: 'Поиск'),
+                const SizedBox(height: 30),
+                EventsCategory(
+                  title: 'Ближайшие мероприятия',
+                  events: state.events,
+                ),
+              ],
+            );
           }
           return const Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+}
+
+class EventsCategory extends StatelessWidget {
+  final String title;
+  final List<EventModel> events;
+
+  const EventsCategory({super.key, required this.title, required this.events});
+
+  Widget eventsMapper(EventModel event) => EventCard(event: event);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 18),
+        GridView.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 25,
+          crossAxisSpacing: 25,
+          shrinkWrap: true,
+          childAspectRatio: 250 / 210,
+          children: events.map(eventsMapper).toList(),
+        ),
+      ],
     );
   }
 }
